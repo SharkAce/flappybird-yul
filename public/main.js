009164ad97ef9ecbd326cfb77b2ec1b8d2028af9
SharkAce <charles.marchildon2@gmail.com>
2021-11-09 18:37:43 -0500

    index.html init

---
+++ public/main.js
@@ -0,0 +1,15 @@
+
+const wn = {
+    x : innerWidth,
+    y : innerHeight
+}
+
+
+function setup(){
+    let canvas = createCanvas(wn.x,wn.y)
+    canvas.parent('canvas')
+}
+
+function draw(){
+background(50)
+}

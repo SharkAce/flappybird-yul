a9745f9776bb5e6716989280b46c5b4b44f6eee3
SharkAce <charles.marchildon2@gmail.com>
2021-11-09 20:19:45 -0500

    Update Pipe.js

--- src/class/Pipe.js
+++ src/class/Pipe.js
@@ -1,11 +1,12 @@
 class Pipe{
     constructor (gapStart, gapSize){
-        this.x = wn.x,
-        this.gapStart = gapStart,
-        this.gapSize = gapSize
+        this.x = wn.x;
+        this.gapStart = gapStart;
+        this.gapSize = gapSize;
+        this.speed = 10;
     }
     update(){
-        this.x-=1
+        this.x-=this.speed;
     }
     render(){
         rect(this.x, 0, pipeWidth, this.gapStart);
